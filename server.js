5a72b45de03ecfa513310cb4908552b3b590d4ac
Anshika <anshikaa052@gmail.com>
2026-06-20 22:02:54 +0530

    Final CORS: Vercel + Localhost allowed

--- server.js
+++ server.js
@@ -8,10 +8,22 @@ const productRoutes = require('./routes/productRoutes');
 
 const app = express();
 
-// SIMPLE CORS - Sab allow kar abhi. Baad me lock karenge
+// FINAL CORS FIX - Localhost + Vercel dono chalega
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://shop-easy-frontend-kdry4x4qa-anshikaa052-orggs-projects.vercel.app'
+];
+
 app.use(cors({
-  origin: true,
-  credentials: true, 
+  origin: function (origin, callback) {
+    // Postman ya server-to-server request ke liye origin null hota hai
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('CORS error: Origin not allowed'));
+    }
+  },
+  credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 }));
@@ -31,7 +43,7 @@ const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
   console.log("MongoDB Connected Successfully");
-  app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 })
 .catch(err => {
   console.log("MongoDB Error:", err);
