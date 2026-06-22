2255eba7c46dd44cc5eda3d43bd756a14810451f
Anshika <anshikaa052@gmail.com>
2026-06-22 12:06:38 +0530

    Remove invalid options handler

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(cors({
 }));
 
 // 👇 Ye line 404 preflight fix karegi - Sabse Important
-app.options('*', cors()); 
+// app.options('*', cors()); 
 
 app.use(express.json());
 app.use('/api/products', productRoutes);
